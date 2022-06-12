9dfa10f11ecd5174111e8869064aaa087f6117a4
unknown <simshengxue1997@gmail.com>
2022-06-12 10:01:21 +0800

    add React Native Elements

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx sssssto start working on your app!</Text>
       <StatusBar style="auto" />
+      <Icon
+        name='sc-telegram'
+        type='evilicon'
+        color='#517fa4'
+      />
     </View>
   );
 }
