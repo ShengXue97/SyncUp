69330bff6de2b6358a94ce6a4346807ddbf31652
unknown <simshengxue1997@gmail.com>
2022-06-13 23:18:37 +0800

    add nice looking calendar

--- pages/CalendarPage.tsx
+++ pages/CalendarPage.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Dimensions } from 'react-native';
+import CalendarStrip from 'react-native-calendar-strip';
+
 const window = Dimensions.get("window");
 const screen = Dimensions.get("screen");
 
@@ -18,41 +20,54 @@ export default function CalendarPage() {
     });
 
     return (
-        <ScrollView>
-            <View style={styles.weekContainer}>
-                {
-                    Array(8).fill(0).map((_, i) =>
-                        <View key={i} style={styles.dayContainer}>
-                            {
-                                Array(24).fill(0).map((_, j) =>
-                                    //Cell container needs to use inline style for dynamic update of width and height during rotation
-                                    <View key={j} style={{
-                                        flex: 1,
-                                        borderWidth: 0.5,
-                                        borderColor: '#D3D3D3',
-                                        width: dimensions.window.width / 8,
-                                        height: dimensions.window.height / 15,
-                                    }}>
-                                        {i == 0 ?
-                                            //First column is special, showing the time
-                                            <Text style={{
-                                                fontSize: 12,
-                                            }}>{j < 10 ? 0 : null}{j}:00</Text>
-                                            : <Text></Text>
-                                        }
-                                    </View>
-                                )
-                            }
-                        </View>
-                    )
-                }
-            </View >
-        </ScrollView >
-
+        <View style={styles.container}>
+            <CalendarStrip
+                style={{ height: 80, paddingTop: 20, paddingBottom: 10 }}
+                calendarColor={'white'}
+                calendarHeaderStyle={{ color: 'black' }}
+                dateNumberStyle={{ color: 'black' }}
+                dateNameStyle={{ color: 'black' }}
+                iconContainer={{ flex: 0.12 }}
+            />
+            <ScrollView>
+                <View style={styles.weekContainer}>
+                    {
+                        Array(9).fill(0).map((_, i) =>
+                            <View key={i} style={[styles.dayContainer, { flex: i == 8 ? 0.7 : i == 0 ? 0.9 : 1 }]}>
+                                {
+                                    Array(24).fill(0).map((_, j) =>
+                                        //Cell container needs to use inline style for dynamic update of width and height during rotation
+                                        <TouchableOpacity key={j} style={{
+                                            flex: 1,
+                                            borderColor: '#D3D3D3',
+                                            borderBottomWidth: i == 8 ? 0 : i == 0 ? 0 : 1,
+                                            borderLeftWidth: 1,
+                                            width: dimensions.window.width / 7.77,
+                                            height: dimensions.window.height / 15,
+                                            backgroundColor: 'white',
+                                        }}>
+                                            {i == 0 ?
+                                                //First column is special, showing the time
+                                                <Text style={{
+                                                    fontSize: 10,
+                                                    textAlign: 'center',
+                                                }}>{j < 10 ? 0 : null}{j}:00</Text>
+                                                : <Text></Text>
+                                            }
+                                        </TouchableOpacity>
+                                    )
+                                }
+                            </View>
+                        )
+                    }
+                </View >
+            </ScrollView >
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
+    container: { flex: 1 },
     weekContainer: {
         flex: 1,
         height: '100%',
@@ -61,7 +76,6 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
     },
     dayContainer: {
-        flex: 1,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
